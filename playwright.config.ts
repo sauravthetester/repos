3c533f91d8291b3f226603bb373356a2662a7f46
sauravthetester <saurav3046g@gmail.com>
2025-02-09 16:25:53 +0530

    playwright trx

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   testMatch: '**/*.spec.ts',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+  reporter: [['playwright-trx-reporter', { outputFile: 'results.trx' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
