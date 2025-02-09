799223c7c88d5213a9f66cfb32ca9800ebb66a50
sauravthetester <saurav3046g@gmail.com>
2025-02-09 17:36:38 +0530

    playwright2 trx reporter

--- playwright2.config.ts
+++ playwright2.config.ts
@@ -23,10 +23,10 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  testMatch: '**/*j.spec.ts',
+  testMatch: '**/*.spec.ts',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  reporter: [['playwright-trx-reporter', { outputFile: 'results.trx' }]],
+  reporter: [['junit', { outputFile: 'results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
