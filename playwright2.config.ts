477a243aac84b2e59528e1e8c47a9e3f63a57185
sauravthetester <saurav3046g@gmail.com>
2025-02-11 01:52:37 +0530

    added dot reporter

--- playwright2.config.ts
+++ playwright2.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   testMatch: '**/*.spec.ts',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'dot',
+  reporter: 'html',
   // reporter: [['junit', { outputFile: 'results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
